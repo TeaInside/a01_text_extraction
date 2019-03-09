ab6458bd145c8c4d6500fe5ae7b3fa77d8e2b2a8
Ammar Faizi <ammarfaizi2@gmail.com>
2019-03-09 14:31:18 +0700

    Fix integral daemon

--- integral2_daemon.php
+++ integral2_daemon.php
@@ -20,19 +20,26 @@ while (true):
 	$found = false;
 	print "Querying group_messages...";
 	$st1 = $pdo1->prepare(
-		"SELECT `t`.`group_id`,`t`.`reply_to_tmsg_id`,`t`.`text` FROM 
-			(SELECT `id` FROM `group_messages` WHERE `reply_to_tmsg_id` IS NOT NULL ORDER BY `id` ASC LIMIT 100 OFFSET {$offset}) AS `u`
-		INNER JOIN `group_messages` AS `t` ON `u`.`id` = `t`.`id`;"
+		"SELECT 	
+			`t`.`group_id` AS `group_id`,
+			`t`.`reply_to_tmsg_id` AS `reply_to_msg_id`,
+			`t`.`tmsg_id` AS `tmsg_id`,
+			`t`.`text` AS `reply`,	
+			`v`.`text` AS `msg`
+		FROM 
+		(SELECT `id` FROM `group_messages` WHERE `reply_to_tmsg_id` IS NOT NULL ORDER BY `id` ASC LIMIT 30 OFFSET {$offset}) AS `u`
+		INNER JOIN `group_messages` AS `t` ON 
+			`u`.`id` = `t`.`id` 
+		INNER JOIN `group_messages` AS `v` ON
+			`v`.`group_id` = `t`.`group_id` AND `v`.`tmsg_id` = `t`.`reply_to_tmsg_id`
+		WHERE `v`.`text` IS NOT NULL;"
 	);
 	print "OK\n";
-
-
 	$st2 = $pdo2->prepare(
-		"INSERT IGNORE INTO `all_text` (`hash_text`, `main_text`, `hit_count`, `created_at`, `updated_at`) VALUES (:hash_text, :main_text, 0, :created_at, NULL);"
+		"INSERT IGNORE INTO `all_text` (`hash_text`, `main_text`, `created_at`) VALUES (:hash_text, :main_text, :created_at);"
 	);
-	$st3 = $pdo1->prepare("SELECT `text` FROM `group_messages` WHERE `group_id` = :group_id AND `tmsg_id` = :tmsg_id LIMIT 1;");
-	$st4 = $pdo2->prepare(
-		"INSERT IGNORE INTO `answers` (`all_text_id`, `hash_text`, `text`, `hit_count`, `created_at`, `updated_at`) VALUES ((SELECT `id` FROM `all_text` WHERE `hash_text` = :hash_all_text LIMIT 1), :hash_text, :_text, 0, :created_at, NULL);"
+	$st3 = $pdo2->prepare(
+		"INSERT IGNORE INTO `replies` (`all_text_id`, `hash_text`, `text`, `created_at`) VALUES ((SELECT `id` FROM `all_text` WHERE `hash_text` = :hash_all_text LIMIT 1), :hash_text, :_text, :created_at);"
 	);
 	print "Analyzing data...\n";
 	$st1->execute();
@@ -40,34 +47,32 @@ while (true):
 	while ($r = $st1->fetch(PDO::FETCH_ASSOC)) {
 		$c++;
 		print "Collecting data...";
-		$st3->execute([":group_id" => $r["group_id"], "tmsg_id" => $r["reply_to_tmsg_id"]]);
-		if ($rr = $st3->fetch(PDO::FETCH_ASSOC)) {
-			$answer_hash = sha1(json_encode(
+		if (strlen($r["reply"]) > 0) {
+			$all_text_hash = sha1($r["msg"]);
+			$reply_hash = sha1(json_encode(
 				[
-					"a" => $r["reply_to_tmsg_id"],
-					"b" => $r["text"],
-					"c" => $r["group_id"]
+					"a" => $all_text_hash,
+					"b" => $r["reply"],
+					"c" => $r["group_id"],
+					"d" => $r["tmsg_id"]
 				]
 			));
-			$all_text_hash = sha1($rr["text"]);
 			$st2->execute(
 				[
 					":hash_text" => $all_text_hash,
-					":main_text" => $rr["text"],
+					":main_text" => $r["msg"],
 					":created_at" => date("Y-m-d H:i:s")
 				]
 			);
-			$st4->execute(
+			$st3->execute(
 				[
 					":hash_all_text" => $all_text_hash,
-					":hash_text" => $answer_hash,
-					":_text" => $r["text"],
+					":hash_text" => $reply_hash,
+					":_text" => $r["reply"],
 					":created_at" => date("Y-m-d H:i:s")
 				]
 			);
 			print "OK\n";
-		} else {
-			print "(No reply) Skip\n";
 		}
 	}
 
